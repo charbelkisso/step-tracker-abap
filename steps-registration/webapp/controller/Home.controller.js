0d7997b52d0099a0ec029c053a32236b6246cbe7
Kisso <charbelkisso@gmail.com>
2021-03-08 16:28:11 +0100

    navigation

--- steps-registration/webapp/controller/Home.controller.js
+++ steps-registration/webapp/controller/Home.controller.js
@@ -1,10 +1,35 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-	"use strict";
+sap.ui.define(
+	["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent"],
+	function (Controller, UIComponent) {
+		"use strict";
 
-	return Controller.extend(
-		"cgi.demo.employee.steps.register.stepsregistration.controller.Home",
-		{
-			onInit: function () {},
-		}
-	);
-});
+		return Controller.extend(
+			"cgi.demo.employee.steps.register.stepsregistration.controller.Home",
+			{
+				onInit: function () {},
+
+				/**
+				 *
+				 * @param {*} oEvent
+				 */
+				onTableItemPress: function (oEvent) {
+					let oRouter = this._getRouter();
+					let sUserID = oEvent
+						.getSource()
+						.getBindingContext()
+						.getObject("UserID");
+					oRouter.navTo(
+						"Register",
+						{
+							UserID: sUserID,
+						},
+						false
+					);
+				},
+				_getRouter() {
+					return UIComponent.getRouterFor(this);
+				},
+			}
+		);
+	}
+);
